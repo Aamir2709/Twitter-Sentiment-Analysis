df77fe74123feeb759b9ba259ada968abda477a8
Aamir2709 <aamirbaugwala@gmail.com>
2023-02-05 14:15:02 +0530

    commit

--- speech.py
+++ speech.py
@@ -1,4 +1,26 @@
-import pandas as pd
+import speech_recognition as sr 
+import moviepy.editor as mp
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 
-df = pd.read_csv('tweets.csv',encoding='ISO-8859-1')
-print(df)
+#52 minutes converted in seconds
+num_seconds_video= 6*60
+print("The video is {} seconds".format(num_seconds_video))
+l=list(range(0,num_seconds_video+1,60))
+print(l)
+
+diz={}
+for i in range(len(l)-1):
+    ffmpeg_extract_subclip("os.mp4", l[i]-2*(l[i]!=0), l[i+1], targetname="chunks/cut{}.mp4".format(i+1))
+    clip = mp.VideoFileClip(r"chunks/cut{}.mp4".format(i+1)) 
+    clip.audio.write_audiofile(r"converted/converted{}.wav".format(i+1))
+    r = sr.Recognizer()
+    audio = sr.AudioFile("converted/converted{}.wav".format(i+1))
+    with audio as source:
+      r.adjust_for_ambient_noise(source)  
+      audio_file = r.record(source)
+    result = r.recognize_google(audio_file)
+    diz['chunk{}'.format(i+1)]=result
+
+l_chunks=[diz['chunk{}'.format(i+1)] for i in range(len(diz))]
+text='\n'.join(l_chunks)
+print(text)
